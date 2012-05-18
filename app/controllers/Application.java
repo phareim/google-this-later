8ead947d434e3c490e80e0c530f2068ebb81e66f
Petter Hareim <phareim@gmail.com>
2012-05-18 22:28:15 +0200

    playing around with the tutorial

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -16,19 +16,27 @@ public class Application extends Controller {
 
   public static Result words(){
   	return ok(
-    views.html.index.render(Word.all(), wordForm)
-  );
+    views.html.index.render(Word.all(), wordForm) );
   }
 
-  public static Result word(Long word){
+  public static Result word(Long id){
   	return TODO;
   }
 
-	public static Result newWord(){
-  	return TODO;
+  public static Result deleteWord(Long id){
+  	 Word.delete(id);
+    return redirect(routes.Application.words());
   }
 
-  public static Result deleteWord(Long word){
-  	return TODO;
+  public static Result newWord() {
+  Form<Word> filledForm = wordForm.bindFromRequest();
+  if(filledForm.hasErrors()) {
+    return badRequest(
+      views.html.index.render(Word.all(), filledForm)
+    );
+  } else {
+    Word.create(filledForm.get());
+    return redirect(routes.Application.words());  
   }
+}
 }
--- app/models/Word.java
+++ app/models/Word.java
@@ -1,24 +1,31 @@
 package models;
 
 import java.util.*;
-
+import play.db.ebean.*;
 import play.data.validation.Constraints.*;
+import javax.persistence.*;
 
-public class Word {
-    
+@Entity
+public class Word extends Model {
+  
+  @Id  
   public Long id;
 
   @Required
   public String label;
-  
+   
   public static List<Word> all() {
-    return new ArrayList<Word>();
+    return find.all();
   }
-  
-  public static void create(Word task) {
+
+  public static void create(Word word) {
+    word.save();
   }
-  
+
   public static void delete(Long id) {
-  }
-    
+    find.ref(id).delete();
+  }  
+  public static Finder<Long,Word> find = new Finder(
+    Long.class, Word.class
+  );  
 }
